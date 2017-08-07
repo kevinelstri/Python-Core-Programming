ea07a5161a4e7976afa687821a1ea47effefbdba
kevinelstri <1098918523@qq.com>
2017-08-07 09:02:27 +0800

    ..

---
+++ 051-number.py
@@ -0,0 +1,62 @@
+﻿# coding:utf-8
+
+# 注意：Python中一切都是对象
+# 创建数字对象
+anInt = 1
+aLong = -999999999999999999L
+aFloat = 3.142567273837287372632763726723837837
+aComplex = 1.23 + 4.56j
+
+# 更新对象
+anInt += 1
+aFloat = 2.34747387483
+
+# 删除对象
+del anInt
+del aLong
+
+# print anInt  # 已删除
+# print aLong  # 已删除
+print aFloat  # 已更新
+print aComplex
+# -------------------------------------------------
+
+# 整型包括：布尔型，标准整形，长整形
+# 整型支持：八进制(以“0”开头)，十进制，十六进制(以“0x”开头)
+ai = 0101010
+bi = 673
+ci = 0x8
+di = 74327823L
+
+# 浮点型：Python中的浮点型全部都是双精度浮点型，可以使用十进制或科学计数法来表示。
+af = 0.0
+bf = -777.
+cf = 3.893
+ef = 67e3
+ff = float(34)
+
+# 复数：复数由实数部和虚数部构成，虚数单位为j
+# 表示为：real+imagej
+# 实数部和虚数部都是浮点数
+ac = 783.3+821.32j
+bc = 326.32-73.32j
+cc = -56.23-635.3j
+dc = -.004+0j
+# 复数的内建属性：real,image,conjugate 共轭复数
+ec = -8.333-2.99j
+print ec.real
+print ec.imag
+print ec.conjugate()  # 返回共轭复数
+# -------------------------------------------------
+
+# 类型转换
+# 整型转换为浮点型，非复数型转换为复数型
+achange = 3627
+bchange = float(achange)
+cchange = 28
+dchange = complex(28)
+print bchange, type(bchange)  # <type 'float'>
+print dchange, type(dchange)  # <type 'complex'>
+
+
+
---
+++ 052-operater.py
@@ -0,0 +1,46 @@
+﻿# coding:utf-8
+
+from __future__ import division
+
+# 操作符
+# 算术操作符：+,-,*,/,%,**
+# 注意：除法
+print 1 / 2
+print float(2 / 3)
+print 3 % 2
+print 7 ** 4
+
+# 位操作符:只适用于整型
+# 取反(~)，按位与(&)，或(|)，异或(^)，左移(<<)，右移(>>)
+print ~30
+print 78 & -32
+print 62 | 82
+print 30 ^ 45
+print 60 >> 2
+print 38 << 3
+
+# 内建函数: cmp(),str(),type()
+# 功能内建函数：abs(),coerce(),divmod(),pow(),round()
+print cmp(-6, 2)
+print cmp(0xff, 255)
+print str(632)
+print type(0xff)
+
+# 工厂函数：数值工厂函数 int(),float(),long(),complex(),bool()
+
+
+# 布尔数：在数学运算中，布尔值的True和False分别对应于1和0
+
+# 十进制浮点型：from decimal import Decimal
+from decimal import Decimal
+dec = Decimal(1.1)
+print dec
+
+# 模块:decimal,array,math,operator,random
+# 核心模块：random
+# |--|--|
+# |randint()||
+# |randrange()||
+# |uniform()||
+# |random()||
+# |choice()||
